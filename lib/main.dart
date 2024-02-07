e79664fd0ce1e636270a2e8519bc6f19d08e3e1f
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-02-07 17:23:46 -0300

    Created the ProductList Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/routes/app_routes.dart';
@@ -13,32 +15,35 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minha Loja',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.purple,
-          secondary: Colors.deepOrange,
-          background: Colors.white,
-        ),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.purple,
-          iconTheme: IconThemeData(
-            color: Colors.white,
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Minha Loja',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+            background: Colors.white,
           ),
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 25,
-            fontFamily: 'Lato',
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.purple,
+            iconTheme: IconThemeData(
+              color: Colors.white,
+            ),
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+              fontFamily: 'Lato',
+            ),
           ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (_) => const ProductDetailPage(),
+        },
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (_) => ProductDetailPage(),
-      },
-      debugShowCheckedModeBanner: false,
     );
   }
 }
