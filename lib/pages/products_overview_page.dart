c03b3b5d9d8e1cebeff19bfc78dddde326f227fe
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-02-07 14:40:13 -0300

    created widget product_item

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/widgets/product_item.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  const ProductsOverviewPage({super.key});
+  final List<Product> loadedProducts = dummyProducts;
+  ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +13,21 @@ class ProductsOverviewPage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        itemCount: loadedProducts.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+        ),
+        itemBuilder: (context, index) {
+          return ProductItem(
+            product: loadedProducts[index],
+          );
+        },
       ),
     );
   }
